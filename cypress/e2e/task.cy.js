8e7844475ec3500af6553d5bd3ed3ce608651896
ruby --version <paulinn.rs@gmail.com>
2023-03-30 11:44:24 -0300

    Consumindo API Rest

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -3,6 +3,15 @@
 describe('tarefas', () => {
 
     it('deve cadastrar uma nova tarefa', () => {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: 'Ler um livro de Node.js'}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })    
+
         cy.visit('http://localhost:8080/')
 
         cy.get('input[placeholder="Add a new Task"]')
