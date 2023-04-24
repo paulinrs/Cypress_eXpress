952ca5c161fe9c859c5dc67254943ea842c80487
ruby --version <paulinn.rs@gmail.com>
2023-04-24 19:09:20 -0300

    Custom Commands

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -6,53 +6,23 @@ describe('tarefas', () => {
 
         const taskName = 'Ler um livro de Node.js'
 
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: taskName }
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })    
+        cy.removeTaskByName(taskName)
+        cy.createTask(taskName)
 
-        cy.visit('http://localhost:8080/')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(taskName)
-        
-        cy.contains('button', 'Create').click()
-
-        cy.contains('main div p', taskName )
+        cy.contains('main div p', taskName)
             .should('be.visible')
     })
 
-    it('não deve permitir tarefa duplicada', ()=> {
-    
+    it('não deve permitir tarefa duplicada', () => {
+
         const task = {
             name: 'comprar um livro de Teste de Software',
             is_done: false
         }
 
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: task.name}
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })   
-        cy.request({
-            url: 'http://localhost:3333/tasks',
-            method: 'POST',
-            body: task
-        }).then(response => {
-            expect(response.status).to.eq(201)
-        })   
-        
-        cy.visit('http://localhost:8080/')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(task.name)
-        
-        cy.contains('button', 'Create').click()
+        cy.removeTaskByName(task.name)
+        cy.postTask(task)
+        cy.createTask(task.name)
 
         cy.get('.swal2-html-container')
             .should('be.visible')
