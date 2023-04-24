312780bccb0a405be51879ad891de81c600b8294
ruby --version <paulinn.rs@gmail.com>
2023-04-24 11:30:08 -0300

    Testes independentes

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -16,10 +16,38 @@ describe('tarefas', () => {
 
         cy.get('input[placeholder="Add a new Task"]')
             .type('Ler um livro de Node.js')
-
-        // //button[contains(text(), "Create")]
         
         cy.contains('button', 'Create').click()
+
+        cy.contains('main div p','Ler um livro de Node.js' )
+            .should('be.visible')
     })
 
+    it('não deve permitir tarefa duplicada', ()=> {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: 'comprar um livro de Teste de Software'}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })   
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: {name: 'comprar um livro de Teste de Software', is_done: false}
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })   
+        
+        cy.visit('http://localhost:8080/')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('comprar um livro de Teste de Software')
+        
+        cy.contains('button', 'Create').click()
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
 })
