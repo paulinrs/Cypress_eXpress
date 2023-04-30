0d4741b30445854fea62810fe967ebce1200bc6d
ruby --version <paulinn.rs@gmail.com>
2023-04-30 08:55:27 -0300

    Organizacao por contexto

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -2,35 +2,36 @@
 
 describe('tarefas', () => {
 
-    it('deve cadastrar uma nova tarefa', () => {
+    context('cadastro', () => {
+        it('deve cadastrar uma nova tarefa', () => {
 
-        const taskName = 'Ler um livro de Node.js'
-
-        cy.removeTaskByName(taskName)
-        cy.createTask(taskName)
-        cy.contains('main div p', taskName)
-            .should('be.visible')
+            const taskName = 'Ler um livro de Node.js'
+    
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
+    
+        it('não deve permitir tarefa duplicada', () => {
+    
+            const task = {
+                name: 'comprar um livro de Teste de Software',
+                is_done: false
+            }
+    
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+    
+        it('campo obrigatório', ()=> {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
     })
-
-    it('não deve permitir tarefa duplicada', () => {
-
-        const task = {
-            name: 'comprar um livro de Teste de Software',
-            is_done: false
-        }
-
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-        cy.createTask(task.name)
-
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
-
-    it('campo obrigatório', ()=> {
-        cy.createTask()
-        cy.isRequired('This is a required field')
-    })  
 })
-
