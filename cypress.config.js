0f059a3f644ae2414afedcbf90dfb32a51971b80
ruby --version <paulinn.rs@gmail.com>
2023-06-05 18:32:58 -0300

    Allure Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -9,7 +10,8 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -18,7 +18,7 @@ describe('tarefas', () => {
 
             cy.removeTaskByName(taskName)
             cy.createTask(taskName)
-            cy.contains('main div p', 'simulando bug')
+            cy.contains('main div p', taskName)
                 .should('be.visible')
         })
 
